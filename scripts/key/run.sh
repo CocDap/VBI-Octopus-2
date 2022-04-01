383423ac63ed2d02f8b6d110cda7817a60ea9947
CocDap <hodinhdung1997@gmail.com>
2022-04-01 16:19:32 +0700

    feat: add key

---
+++ scripts/key/run.sh
@@ -0,0 +1,5 @@
+curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@aura1"
+curl http://localhost:9934  -H "Content-Type:application/json;charset=utf-8" -d "@aura2"
+
+curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@gran1"
+curl http://localhost:9934  -H "Content-Type:application/json;charset=utf-8" -d "@gran2"
